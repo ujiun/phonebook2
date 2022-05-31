eb04e7481af89d57a528bb904327b112d6bb44f4
ujiun <ujiun@hanmail.net>
2022-05-31 13:03:18 +0900

    phonebook2

--- src/com/javaex/util/WebUtil.java
+++ src/com/javaex/util/WebUtil.java
@@ -19,16 +19,16 @@ public class WebUtil {
 	
 	//포워드
 	public static void forward(HttpServletRequest request,
-						HttpServletResponse response,
-						String path) throws ServletException, IOException{
+							   HttpServletResponse response,
+							   String path) throws ServletException, IOException{
 		RequestDispatcher rd = request.getRequestDispatcher(path);
 		rd.forward(request, response);
 	}
 	
 	//리다이렉트
 	public static void redirect(HttpServletRequest request,
-						 HttpServletResponse response,
-					     String url) throws IOException{
+								HttpServletResponse response,
+								String url) throws IOException{
 		response.sendRedirect(url);
 	}
 	
